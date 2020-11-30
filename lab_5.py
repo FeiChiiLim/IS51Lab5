521b404117ee5ac6613eecded6d041f13963feb5
Raven <raven@Ravens-MacBook-Air.local>
2020-11-30 17:06:47 +0800

    finish pseudo-code

--- lab_5.py
+++ lab_5.py
@@ -15,3 +15,25 @@ in our dictionary.
 After each word is translated, we then 
 print out the translated sentence to the user.
 """
+
+
+"""
+main():
+  set sentence = input()
+  set dictionary = create_dictionary()
+  translate(sentence, dictionary)
+
+translate(sentence, dictionary):
+  words = for each of the  word in the sentence
+  for each words, translate the word
+  print translated sentence to user
+
+create_dictionary():
+  read in textese.txt
+  create list = each line from the file
+  close the file
+  create a dict off of the list
+  return dict
+
+main()
+"""
